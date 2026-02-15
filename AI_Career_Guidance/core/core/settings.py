ea9a2ad574ee084d27ab3ddbc34ff044ff10efab
Deepanshu Patyal <patyaldeepanshu05@gmail.com>
2026-02-16 02:34:05 +0530

    again changes

--- AI_Career_Guidance/core/core/settings.py
+++ AI_Career_Guidance/core/core/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import dj_database_url
 import os
 from pathlib import Path
 from turtle import setx
@@ -81,17 +82,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
  
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'careerapp',
-            'USER': 'postgres',
-            'PASSWORD': 'Patyal@2005',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
-    }
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'careerapp',
+#             'USER': 'postgres',
+#             'PASSWORD': 'Patyal@2005',
+#             'HOST': 'localhost',
+#             'PORT': '5432',
+#         }
+#     }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
