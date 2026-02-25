ada70f7f7c803b8530f4c33b65d7f03904b38a49
Deepanshu Patyal <patyaldeepanshu05@gmail.com>
2026-02-26 01:54:09 +0530

    deploy ready

--- AI_Career_Guidance/core/core/settings.py
+++ AI_Career_Guidance/core/core/settings.py
@@ -13,8 +13,7 @@ import dj_database_url
 import os
 from pathlib import Path
 
-GROQ_API_KEY= "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"
-
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 
  
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
