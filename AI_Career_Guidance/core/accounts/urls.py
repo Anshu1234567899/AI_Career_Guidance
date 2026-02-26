48a12511c76298eb125cec987d442d42f179cfc4
Deepanshu Patyal <patyaldeepanshu05@gmail.com>
2026-02-26 18:48:42 +0530

    ai changes

--- AI_Career_Guidance/core/accounts/urls.py
+++ AI_Career_Guidance/core/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import (
     home, login_view, register_view, dashboard, logout_view,
-    edit_profile, career_detail,admin_quiz_result_delete,about_us,career_result,admin_student_profiles,run_migrations,create_superuser,
+    edit_profile, career_detail,admin_quiz_result_delete,about_us,career_result,admin_student_profiles,
     # admin panel views
     admin_dashboard, admin_users, admin_careers,contact_view,career_quiz,admin_quiz_add,admin_quiz_edit,skill_based_careers,admin_categories,edit_category,delete_category,admin_quiz_results,
     admin_user_edit, admin_user_delete,admin_users_bulk_delete,admin_skills_bulk_delete,admin_quiz_list,admin_quiz_delete,edit_account,download_career_pdf,download_personalized_report,
@@ -52,8 +52,8 @@ urlpatterns = [
     path('admin-panel/students/', admin_student_profiles, name='admin_student_profiles'),
 
 
-    path('run-migrations/', run_migrations),
-    path('create-superuser/', create_superuser, name='create_superuser'),
+    # path('run-migrations/', run_migrations),
+    # path('create-superuser/', create_superuser, name='create_superuser'),
 ]
 
 
