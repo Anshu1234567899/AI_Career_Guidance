4964b5b7be0b6b7ffc13ca3a4f34acd43b1431e2
Deepanshu Patyal <patyaldeepanshu05@gmail.com>
2026-03-01 15:07:58 +0530

    false

--- AI_Career_Guidance/core/core/settings.py
+++ AI_Career_Guidance/core/core/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=inh##wzrijvglx4ak3a&%pkp1s%(7a$mlq+)wpxix(dxe%8&y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*'] 
 
